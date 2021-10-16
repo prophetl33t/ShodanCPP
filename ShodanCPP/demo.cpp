388551f2458aee2af6fcf716d19614226a0ab82c
Entity.Human <29117146+prophetl33t@users.noreply.github.com>
2021-10-16 23:24:38 +0300

    Usage examples improvmements and curl fixes
    Demo as a compilable executable
    New example without api key
    Add CMake var for curl libs path
    Add libs needed on win32 for curl

--- ShodanCPP/demo.cpp
+++ ShodanCPP/demo.cpp
@@ -1,15 +1,37 @@
 #include "ShodanClient.h"
 
-int main(void)
+//demo without api key
+void Demo1()
+{
+    ShodanClient cl;
+    std::cout << "Your IP address is: " << cl.GetMyIP() << '\n';
+}
+
+//demo with api key
+void Demo2(nonstd::string_view s)
 {
     //Print information about the API plan.
     ShodanClient cl;
-    cl.SetAPIKey("insert api key here");
+    cl.SetAPIKey(s);
     std::cout << cl.GetAPIInfo() << "\n";
 
     //Print the number of devices that have the string "webcam" in their banner
     rapidjson::Document doc;
     doc.Parse(cl.GetHostCount("webcam").c_str());
     std::cout << doc["total"].GetInt() << "\n";
+}
+
+int main(void)
+{
+    Demo1();
+
+    std::string key;
+    std::cout << "Enter your api key:\n";
+    std::cin >> key;
+    Demo2(key);
+
+    std::cout << "Press any key to continue...";
+    std::cin.get();
+    std::cin.get();
     return 0;
 }
