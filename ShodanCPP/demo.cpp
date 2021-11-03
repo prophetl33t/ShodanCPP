af839c86ca43511242c6d92445301412c7aee727
Stanislav Potanin (prophetl33t) <29117146+prophetl33t@users.noreply.github.com>
2021-11-03 15:52:51 +0700

    update ca bundle and move it to working dir for better debugging in VS

--- ShodanCPP/demo.cpp
+++ ShodanCPP/demo.cpp
@@ -13,12 +13,12 @@ void Demo2(nonstd::string_view s)
     //Print information about the API plan.
     ShodanClient cl;
     cl.SetAPIKey(s);
-    std::cout << cl.GetAPIInfo() << "\n";
+    std::cout << cl.GetAPIInfo() << '\n';
 
     //Print the number of devices that have the string "webcam" in their banner
     rapidjson::Document doc;
     doc.Parse(cl.GetHostCount("webcam").c_str());
-    std::cout << doc["total"].GetInt() << "\n";
+    std::cout << doc["total"].GetInt() << '\n';
 }
 
 int main(void)
